38ae135b77ad1ed9d9f568bc750c39241d6f416c
Terry <82017869+2218084076@users.noreply.github.com>
2022-05-25 09:05:03 +0800

    daily ：daily update

--- proxypool/proxypool/spiders/spider.py
+++ proxypool/proxypool/spiders/spider.py
@@ -7,10 +7,7 @@ class Spider(scrapy.spiders.Spider):
     name = 'mimvp'
     allowed_domains = ['mimvp.org']
     start_urls = [
-        'https://proxy.mimvp.com/freeopen?proxy=out_hp',
-        'https://proxy.mimvp.com/freeopen?proxy=out_hp&sort=p_ip&page=2',
-        'https://proxy.mimvp.com/freeopen?proxy=out_hp&sort=p_ip&page=3',
-        'https://proxy.mimvp.com/freeopen?proxy=out_hp&sort=p_ip&page=4',
+        'https://proxy.mimvp.com/freeopen?proxy=out_hp&sort=p_ip&page=1',
 
     ]
 
@@ -18,6 +15,6 @@ class Spider(scrapy.spiders.Spider):
         for sel in response.xpath('/html/body/div[2]/div/table/tbody/tr'):
             item = ProxypoolItem()  # 将其在setting中 指定为utf-8，JSON 输出将不会被转义
             item['ip'] = sel.xpath('td[2]/text()').extract()[0]
-            item['country'] = sel.xpath('td[5]/text()').extract()[0]
+            item['country'] = sel.xpath('td[6]/text()').extract()[0]
             item['post'] = sel.xpath('td[4]/text()').extract()[0]
             yield item
